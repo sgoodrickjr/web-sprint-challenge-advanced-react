583e1307ede9d77d4f44fd0833c139414bd00a99
Stephen Goodrick <sgoodrickjr@gmail.com>
2020-06-16 00:12:48 -0400

    tests

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,29 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import App from './App';
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+      // Arrange
+  const { getByText } = render(<App />);
 
-test("form shows success message on submit with form details", () => {});
+  // Act
+  getByText(/cart/i);
+  // test passes if ^ is truthy
+
+  // Assert
+});
+
+test("form shows success message on submit with form details", () => {
+      // Arrange
+  const { getByTestId } = render(<App />);
+
+  // Act
+  getByTestId(/successMessage/i);
+  // test passes if ^ is truthy
+
+  // Assert
+
+});
